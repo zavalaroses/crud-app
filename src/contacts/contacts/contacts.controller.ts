42136effdad377de7a1d15f845c8a80cb8233f33
zavalaroses <zavalaroses@gmail.com>
2023-03-16 13:45:09 -0600

    first commit

---
+++ src/contacts/contacts/contacts.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Post,Put,Delete,Body,Param } from '@nestjs/common';
+import { identity } from 'rxjs';
+import { Contact } from '../contact.entity';
+import { ContactsService } from '../contacts/contacts.service';
+
+
+@Controller('contacts')
+export class ContactsController {
+    constructor(private contactService:ContactsService){}
+    @Get()
+    index():Promise<Contact[]>{
+        return this.contactService.findAll();
+    }
+    @Post('create')
+    async create(@Body() contactData: Contact):Promise<any>{
+        return this.contactService.create(contactData);
+    }
+    @Put(':id/update')
+    async update(@Param('id') id, @Body() contactData: Contact): Promise<any> {
+        contactData.id = Number(id);
+        console.log('Update #' + contactData.id)
+        return this.contactService.update(contactData);
+    }
+    @Delete(':id/delete')
+    async delete(@Param('id') id):Promise<any>{
+        return this.contactService.delete(id);
+    }
+
+}
