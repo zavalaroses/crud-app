6211af9cf39b25379e41a8f654cb00937925098f
zavalaroses <zavalaroses@gmail.com>
2023-03-17 12:25:52 -0600

    eneableCors

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactsModule } from './contacts/contacts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import {Contact } from './contacts/contact.entity';
 
 @Module({
   imports: [ContactsModule,
@@ -12,11 +13,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     username: 'root',
     password: null,
     database:'db',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    entities: [Contact],
     synchronize:true,
   }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
+//__dirname + '/**/*.entity{.ts,.js}'
 export class AppModule {}
